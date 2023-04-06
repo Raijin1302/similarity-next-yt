2c0a06d2329486850f90e176fba6b9a8dc8cf50b
Raijin1302 <raijinnguyen1302@gmail.com>
2023-04-06 23:36:36 +0700

    :hammer: :green_circle: done animated Documentation

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,7 +1,23 @@
 import { FC } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs";
+import Code from "./ui/Code";
+import { nodejs, python } from "@/constant/documentation-code";
 
 const DocumentationTabs: FC = ({}) => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">
+        <Code animated language="javascript" code={nodejs} show />
+      </TabsContent>
+      <TabsContent value="python">
+        <Code animated language="python" code={python} show />
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
