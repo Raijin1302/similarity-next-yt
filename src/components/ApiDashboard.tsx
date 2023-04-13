66ef08c80e0bbb474aefc0c3294750ac0932e35b
Raijin1302 <raijinnguyen1302@gmail.com>
2023-04-13 23:42:56 +0700

    :red_circle: create ApiDashboard

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -1,9 +1,53 @@
-import { FC } from "react";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { formatDistance } from "date-fns";
+import LgHeading from "./ui/LgHeading";
+import Paragraph from "./ui/Paragraph";
+import { Input } from "./ui/Input";
+const ApiDashboard = async () => {
+  const user = await getServerSession(authOptions);
+  if (!user) notFound();
 
-interface ApiDashboardProps {}
+  const apiKeys = await db.apiKey.findMany({
+    where: { userId: user.user.id },
+  });
 
-const ApiDashboard: FC<ApiDashboardProps> = ({}) => {
-  return <div>ApiDashboard</div>;
+  const activatedApiKey = apiKeys.find((apiKey) => apiKey.enabled);
+
+  console.log(apiKeys);
+
+  if (!activatedApiKey) notFound();
+  const userRequests = await db.apiRequest.findMany({
+    where: {
+      apiKeyId: {
+        in: apiKeys.map((key) => key.id),
+      },
+    },
+  });
+
+  const serializabelRequests = userRequests.map((req) => ({
+    ...req,
+    timestamp: formatDistance(new Date(req.timestamp), new Date()),
+  }));
+  return (
+    <div className="container flex flex-col gap-6">
+      <LgHeading>Welcome back, {user.user.name}</LgHeading>
+      <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
+        <Paragraph>Your APi key :</Paragraph>
+        <Input
+          className="w-fit truncate"
+          readOnly
+          value={activatedApiKey.key}
+        />
+        {/* add options to create / */}
+      </div>
+      <Paragraph className="text-center md:text-left mt-4 -mb-4">
+        Your APi history :
+      </Paragraph>
+    </div>
+  );
 };
 
 export default ApiDashboard;
