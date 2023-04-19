f4901cebcf82a9682347f8010af73207c8c76a3a
Raijin1302 <raijinnguyen1302@gmail.com>
2023-04-19 23:25:06 +0700

    :red_circle: remove semicolon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,20 @@
-
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import ThemeToggle from "./ThemeToggle";
-import SignOutButton from "@/components/SignOutButton";
-import SignInButton from "@/components/SignInButton";
-import { buttonVariants } from "./ui/Button";
-import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth"
+import Link from "next/link"
+import ThemeToggle from "./ThemeToggle"
+import SignOutButton from "@/components/SignOutButton"
+import SignInButton from "@/components/SignInButton"
+import { buttonVariants } from "./ui/Button"
+import { authOptions } from "@/lib/auth"
 
 const Navbar = async ({}) => {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 inset-x-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
         <Link href="/" className={buttonVariants({ variant: "link" })}>
           Text Similarity 2.0
         </Link>
-        <div className="md:hidden ">
+        <div className="md:hidden">
           <ThemeToggle />
         </div>
 
@@ -43,7 +42,7 @@ const Navbar = async ({}) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
