dfbe11c059a6feae85a6c44608f721d7beb063fc
Raijin1302 <raijinnguyen1302@gmail.com>
2023-04-21 23:51:37 +0700

    :red_circle: :hammer: just test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,50 @@ import Link from "next/link"
 import EarthCanvas from "@/components/ui/Earth"
 import { motion } from "framer-motion"
 import { slideIn } from "@/helpers/motion"
+import { useState, useEffect } from "react"
+import Loading from "./loading"
 
 export default function Home() {
+  const [loadingScreen, setLoadingScreen] = useState<boolean>(false)
+
+  useEffect(() => {
+    setLoadingScreen(true)
+    const timerId = setTimeout(() => {
+      setLoadingScreen(false)
+    }, 3500)
+
+    return () => clearTimeout(timerId)
+  }, [])
   return (
     <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
-      <div className="container pt-32 max-w-7xl mx-auto  w-full h-full">
-        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
-          <LgHeading
-            size="lg"
-            className="three-d text-black dark:text-light-gold"
-          >
-            Easily determine <br /> text similarity
-          </LgHeading>
-          <Paragraph className="max-w-xl lg:text-left">
-            With the Text Similarity API, you can easily determine the
-            similarity between two pieces of text with a free{" "}
-            <Link
-              href="/login"
-              className="underline underline-offset-2 text-black dark:text-light-gold"
+      {loadingScreen ? (
+        <Loading />
+      ) : (
+        <div className="container pt-32 max-w-7xl mx-auto  w-full h-full">
+          <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+            <LgHeading
+              size="lg"
+              className="three-d text-black dark:text-light-gold"
             >
-              API key
-            </Link>
-            .
-          </Paragraph>
-          <motion.div
-            variants={slideIn("right", "tween", 0.2, 1)}
-            className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute
+              Easily determine <br /> text similarity
+            </LgHeading>
+            <Paragraph className="max-w-xl lg:text-left">
+              With the Text Similarity API, you can easily determine the
+              similarity between two pieces of text with a free{" "}
+              <Link
+                href="/login"
+                className="underline underline-offset-2 text-black dark:text-light-gold"
+              >
+                API key
+              </Link>
+              .
+            </Paragraph>
+            <motion.div
+              variants={slideIn("right", "tween", 0.2, 1)}
+              className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute
           "
-          >
-            {/* <Image
+            >
+              {/* <Image
               priority
               className="img-shadow"
               quality={100}
@@ -43,10 +58,11 @@ export default function Home() {
               src="/typewriter.png"
               alt="typewriter"
             /> */}
-            <EarthCanvas />
-          </motion.div>
+              <EarthCanvas />
+            </motion.div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
